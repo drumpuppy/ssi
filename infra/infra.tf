9a2762d6ad5bdd47f03a5e7739841d011c223017
etienne <etienne.lazarz@mytower.co>
2024-10-29 17:55:37 +0100

    fixed config

--- infra/infra.tf
+++ infra/infra.tf
@@ -9,46 +9,88 @@ terraform {
 
 variable "project_id" {
   type        = string
-  description = "Your project ID."
+  description = "Your Scaleway project ID."
 }
+
+variable "region" {
+  type        = string
+  default     = "fr-par"
+  description = "Scaleway region."
+}
+
+variable "zone" {
+  type        = string
+  default     = "fr-par-1"
+  description = "Scaleway zone."
+}
+
 
 provider "scaleway" {
-  project_id = var.project_id
-  region     = "fr-par"
-  zone       = "fr-par-1"
+  region = "fr-par"
+  zone   = "fr-par-1"
 }
 
-# Create Private Network
+# Private Virtual Network
 resource "scaleway_vpc_private_network" "pvn" {
-  project_id = var.project_id
   name       = "k8s-private-network"
+  project_id = var.project_id
 }
 
 # Kubernetes Cluster
 resource "scaleway_k8s_cluster" "cluster" {
-  name                = "k8s-cluster"
-  version             = "1.30.2"
-  cni                 = "cilium"
-  private_network_id  = scaleway_vpc_private_network.pvn.id
+  name        = "k8s-cluster"
+  version     = "1.30.2"
+  cni         = "cilium"
+  region      = var.region
+  description = "Scaleway Kubernetes Cluster"
+  type        = "kapsule"
+
+  auto_upgrade {
+    enable                      = false
+    maintenance_window_day      = "sunday"
+    maintenance_window_start_hour = "03:00"
+  }
+
+  autoscaler_config {
+    balance_similar_node_groups = true
+  }
+
   delete_additional_resources = false
+  private_network_id          = scaleway_vpc_private_network.pvn.id
 }
 
-# Kubernetes Pool (Nodes)
+# Kubernetes Pool
 resource "scaleway_k8s_pool" "pool" {
-  cluster_id   = scaleway_k8s_cluster.cluster.id
-  name         = "k8s-pool"
-  node_type    = "DEV1-M"
-  size         = 2
-  autoscaling  = false
-  autohealing  = true
+  cluster_id        = scaleway_k8s_cluster.cluster.id
+  name              = "k8s-pool"
+  node_type         = "PLAY2-NANO"
+  size              = 1
+  min_size          = 1
+  max_size          = 1
+  autohealing       = true
+  autoscaling       = false
+  container_runtime = "containerd"
+  zone              = var.zone
+
+  upgrade_policy {
+    max_surge       = 1
+    max_unavailable = 1
+  }
 }
 
-# Output kubeconfig for GitHub Actions
-output "kubeconfig" {
-  value = jsonencode({
-    host                   = scaleway_k8s_cluster.cluster.kubeconfig[0].host
-    token                  = scaleway_k8s_cluster.cluster.kubeconfig[0].token
-    cluster_ca_certificate = scaleway_k8s_cluster.cluster.kubeconfig[0].cluster_ca_certificate
-  })
-  sensitive = true
+# Load Balancer
+resource "scaleway_lb" "lb" {
+  name                    = "k8s-lb"
+  description             = "Load Balancer for Kubernetes"
+  type                    = "lb-s"
+  project_id              = var.project_id
+  ip_id                   = scaleway_lb_ip.lb_ip.id
+  zone                    = var.zone
+  ssl_compatibility_level = "intermediate"
+}
+
+# Load Balancer IP
+resource "scaleway_lb_ip" "lb_ip" {
+  project_id = var.project_id
+  zone       = var.zone
 }
